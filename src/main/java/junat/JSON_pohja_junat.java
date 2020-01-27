9c6d0864dbce973471233bf381a7fda488cc242c
Lauri Ali-Hokka <laurialihokka@gmail.com>
2020-01-27 14:12:08 +0200

    pari hakutestiä lisätty, toiminnan kannalta ei välttämättömiä

--- src/main/java/junat/JSON_pohja_junat.java
+++ src/main/java/junat/JSON_pohja_junat.java
@@ -10,22 +10,31 @@ import java.util.List;
 
 public class JSON_pohja_junat {
     public static void main(String[] args) {
-        lueJunanJSONData();
+        lueJunanJSONData(265);
     }
 
 
-    private static void lueJunanJSONData() {
+    public static void lueJunanJSONData(int junannumero) { //edellisestä devistä lisätty metodi vastaanottamaan int junannumero (ei välttämätön)
         String baseurl = "https://rata.digitraffic.fi/api/v1";
+
         try {
             URL url = new URL(URI.create(String.format("%s/live-trains/station/HKI/ROI", baseurl)).toASCIIString());
             ObjectMapper mapper = new ObjectMapper();
             CollectionType tarkempiListanTyyppi = mapper.getTypeFactory().constructCollectionType(ArrayList.class, Juna.class);
             List<Juna> junat = mapper.readValue(url, tarkempiListanTyyppi);  // pelkkä List.class ei riitä tyypiksi
-            System.out.println(junat.get(0).getTrainNumber());
-            // Seuraavaa varten on toteutettava TimeTableRow luokka:
-            System.out.println(junat.get(0).getTimeTableRows().get(2).getScheduledTime());
+
+            int i = 0;
+            while(i < junat.size()) {                                           //hakujen testailua proto
+                if (junat.get(i).getTrainNumber() == junannumero) {
+                    System.out.println(junat.get(i).getTimeTableRows().get(0).getScheduledTime());
+                }
+                i++;
+            }
+
+            System.out.println(junat.get(0).getTimeTableRows().get(0).getScheduledTime());
             System.out.println("\n\n");
             System.out.println(junat.get(0));
+
 
         } catch (Exception ex) {
             System.out.println(ex);
--- src/main/java/junat/Main.java
+++ src/main/java/junat/Main.java
@@ -2,8 +2,11 @@ package junat;
 
 public class Main {
     public static void main(String[] args) {
-        Juna juna = new Juna();
+        //Juna juna = new Juna();
         JSON_pohja_junat testi = new JSON_pohja_junat();
-        System.out.println(juna.toString());
+        //System.out.println(juna.toString());//
+
+        testi.lueJunanJSONData(265);
+
     }
 }
