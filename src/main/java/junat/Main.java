93ba3401e5422ba4fabd610e1e75b4176ef45097
Lauri Ali-Hokka <laurialihokka@gmail.com>
2020-01-27 16:08:33 +0200

    käyttöliittymän pohja lisätty, käyttöliittymä saa junadataa

--- src/main/java/junat/Main.java
+++ src/main/java/junat/Main.java
@@ -1,12 +1,12 @@
 package junat;
 
+import java.util.List;
+
 public class Main {
+
+
     public static void main(String[] args) {
-        //Juna juna = new Juna();
-        JSON_pohja_junat testi = new JSON_pohja_junat();
-        //System.out.println(juna.toString());//
 
-        testi.lueJunanJSONData(265);
-
+        Kayttoliittyma.kaynnistaValikko();
     }
 }
